a6ce71781281ba2b74e320b7f34b6f794a4b6a94
Chen, Zhao <zchen827@gatech.edu>
2022-07-23 16:12:06 -0400

    tutorial 4

---
+++ frontend/src/components/App.js
@@ -0,0 +1,25 @@
+import { Home } from "@material-ui/icons";
+import React, {Component} from "react";
+import { render } from "react-dom";
+import HomePage from "./Homepage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div>
+                <HomePage />
+                <RoomJoinPage />
+                <CreateRoomPage />
+            </div>
+        );
+    }
+}
+
+const appDiv = document.getElementById("app");
+render(<App/>, appDiv);
